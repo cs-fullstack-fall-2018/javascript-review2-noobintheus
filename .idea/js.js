42e805956b45534f350887e52ccda9963a6c2341
student <you@example.com>
2018-08-16 14:09:48 -0500

    check in

---
+++ .idea/js.js
@@ -0,0 +1,106 @@
+function main() {
+    var studentName = "Kenn";
+    printName(studentName)
+alert("hello" + studentName)
+}
+
+main();
+
+
+
+
+
+function testNum(a) {
+    if (a > 0) {
+        alert("positive");
+    } else {
+        alert("NOT positive");
+    }
+}
+
+console.log(testNum(-5));
+
+
+while (i < -10) {
+    text += "The number is " + i;
+    i++;
+}
+
+
+function main(){
+var person = prompt("Please enter your favorite number");
+function aboveorbelow100()
+{alert(prompt)
+    if(aboveorbelow100 > 100){
+        alert("over")
+    }
+else{
+    alert("under")
+    }
+}
+
+main();}
+
+
+
+// function main() {
+// }
+//
+// main();
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// function main() {
+// }
+//
+// main();
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// function main() {
+// }
+//
+// main();
+//
+//
+//
+//
+//
